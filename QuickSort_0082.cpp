828598efab508e2b1cea11e892e5dd675919decc
salmaarayyan <salmaarayyan@gmail.com>
2024-03-29 18:15:24 +0700

    membuat variabel global

---
+++ QuickSort_0082.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//array of integers to hold values
+//mendeklarasikan variabel global
+int arrr[20];
+int cmp_count = 0; //jumlah komparasi
+int mov_count = 0;  //jumlah gerak data
+int n;
