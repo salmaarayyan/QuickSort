055e86694b72e75a90117a7715577aebdad5c48c
salmaarayyan <salmaarayyan@gmail.com>
2024-03-29 19:07:44 +0700

    menampilkan nilai arr[i]

--- QuickSort_0082.cpp
+++ QuickSort_0082.cpp
@@ -80,5 +80,7 @@ void display() {
     cout << "sorted array" << endl;
     cout << "----------------------" << endl;
 
-    for (int i = 0; i < n; i++) {}
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 }
